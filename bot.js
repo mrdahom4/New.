d5ce9ba698c552167b9fc8dd64d36d927414e6a2
Lorans <44880953+mrdahom4@users.noreply.github.com>
2019-02-08 08:02:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -674,11 +674,54 @@ client.on('voiceStateUpdate', (voiceOld, voiceNew) => {
     } //Toxic Codes    //  PremiumBot
 }); //Toxic Codes    //  PremiumBot //Toxic Codes    //  PremiumBot //Toxic Codes    //  PremiumBot //Toxic Codes    //  PremiumBot //Toxic Codes    //  PremiumBot
 
-
-client.on('message', msg => {
-  if (msg.content === 't!avatar') {
-    msg.channel.send('${client.avatarURL}');
-  }
+client.on("message", message => {
+const sm = require("string-similarity");
+let members = [];
+let indexes = [];
+ 
+let args = message.content.split(" ");
+let prefix = "t!";
+if(args[0].toLowerCase() === (prefix + "avatar")){
+   let members = [];
+let indexes = [];
+ let mentionn = message.mentions.users.first();
+     if(!args[1]) {
+  let x = new Discord.RichEmbed()
+  x.setDescription(`${message.author}'s Profile picture :frame_photo:`)
+  x.setImage(`${message.author.avatarURL}`)
+  message.channel.send(x)
+ }
+ 
+ if(mentionn){
+ 
+ 
+ 
+  let m = new Discord.RichEmbed();
+m.setDescription(`${mentionn}'s Profile picture :frame_photo:`)
+m.setImage(`${mentionn.avatarURL}`)
+message.channel.send(m)
+ }
+ 
+ 
+ 
+ if(!args[1].includes(mentionn)) {
+ message.guild.members.forEach(function(member){
+     members.push(member.user.username);
+     indexes.push(member.id);
+ })
+ 
+ let match = sm.findBestMatch(args.join(' '), members)
+ 
+ let username = match.bestMatch.target;
+ 
+ let member = message.guild.members.get(indexes[members.indexOf(username)]);
+ 
+ let embed = new Discord.RichEmbed()
+ embed.setDescription(`${member}'s Profile picture :frame_photo: `)
+ embed.setImage(`${member.user.avatarURL}`)
+ message.channel.send(embed)
+}
+}
 });
 
 client.on('message', message => {
@@ -1005,6 +1048,140 @@ client.on('message' , message => {
    
    
 
-
+const config = require("./config.json");
+const SQLite = require("better-sqlite3");
+const sql = new SQLite('./scores.sqlite');
+ 
+client.on("ready", () => {
+  // Check if the table "points" exists.
+  const table = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'scores';").get();
+  if (!table['count(*)']) {
+    // If the table isn't there, create it and setup the database correctly.
+    sql.prepare("CREATE TABLE scores (id TEXT PRIMARY KEY, user TEXT, guild TEXT, points INTEGER, level INTEGER);").run();
+    // Ensure that the "id" row is always unique and indexed.
+    sql.prepare("CREATE UNIQUE INDEX idx_scores_id ON scores (id);").run();
+    sql.pragma("synchronous = 1");
+    sql.pragma("journal_mode = wal");
+  }
+  // And then we have two prepared statements to get and set the score data.
+  client.getScore = sql.prepare("SELECT * FROM scores WHERE user = ? AND guild = ?");
+  client.setScore = sql.prepare("INSERT OR REPLACE INTO scores (id, user, guild, points, level) VALUES (@id, @user, @guild, @points, @level);");
+ 
+  // This event will run if the bot starts, and logs in, successfully.
+  console.log('\x1b[32m%s\x1b[0m',`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+  // Example of changing the bot's playing game to something useful. `client.user` is what the
+  // docs refer to as the "ClientUser".
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+ 
+});
+ 
+client.on("guildCreate", guild => {
+  // This event triggers when the bot joins a guild.
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+ 
+client.on("guildDelete", guild => {
+  // this event triggers when the bot is removed from a guild.
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+ 
+client.on("message", async message => {
+  // Ignore bots, DMs and group messages.
+  if (message.author.bot || !message.guild) return;
+ 
+  /*
+    START Points CODE
+  */
+ 
+  // Initialize ("declare") the points. If we did this in the condition it would not be
+  // available later in commands. Because "scopes"!
+  let score;
+ 
+  if (message.guild) {//Baron#1500
+    // Try to get the current user's score.
+    score = client.getScore.get(message.author.id, message.guild.id);//Baron#1500
+ 
+    // If the score doesn't exist (new user), initialize with defaults.
+    if (!score) {
+      score = { id: `${message.guild.id}-${message.author.id}`, user: message.author.id, guild: message.guild.id, points: 0, level: 1 };
+    }
+ 
+    // Increment points.
+    score.points++;
+ 
+    // Calculate the current level through MATH OMG HALP.
+    const curLevel = Math.floor(0.1 * Math.sqrt(score.points));//Baron#1500
+ 
+    // Check if the user has leveled up, and let them know if they have:
+    if(score.level < curLevel) {
+      // Level up!
+      message.reply(`You've leveled up to level **${curLevel}**! Ain't that dandy?`);//Baron#1500
+    }
+ 
+    // Save data to the sqlite table.
+    // This looks super simple because it's calling upon the prepared statement!
+    client.setScore.run(score);
+  }
+    // Ignore any message that does not start with our prefix,
+  if (message.content.indexOf(config.prefix) !== 0) return;//Baron#1500
+ 
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+ 
+  if(command === "points") {
+    return message.reply(`You currently have ${score.points} points and are level ${score.level}!`);//Baron#1500
+  }
+//Baron#1500
+  if(command === "give") {   //Baron#1500
+    // Limited to guild owner - adjust to your own preference!   //Baron#1500
+    if(!message.author.id === message.guild.owner) return message.reply("You're not the boss of me, you can't do that!");//Baron#1500
+ 
+    // Try to get the user from mention. If not found, get the ID given and get a user from that ID.
+    const user = message.mentions.users.first() || client.users.get(args[0]);
+    if(!user) return message.reply("You must mention someone or give their ID!");//Baron#1500
+ 
+    // Read the amount of points to give to the user.
+    const pointsToAdd = parseInt(args[1], 10);
+    if(!pointsToAdd) return message.reply("You didn't tell me how many points to give...");//Baron#1500
+ 
+    // Get their current points. This can't use `score` because it's not the same user ;)//Baron#1500
+    let userscore = client.getScore.get(user.id, message.guild.id);
+ 
+    // It's possible to give points to a user we haven't seen, so we need to initiate defaults here too!
+    if (!userscore) {
+      userscore = { id: `${message.guild.id}-${user.id}`, user: user.id, guild: message.guild.id, points: 0, level: 1 };//Baron#1500
+    }
+ 
+    // Increment the score.
+    userscore.points += pointsToAdd;
+ 
+    // We also want to update their level (but we won't notify them if it changes)
+    let userLevel = Math.floor(0.1 * Math.sqrt(score.points));
+    userscore.level = userLevel;
+ 
+    // And we save it!
+    client.setScore.run(userscore);
+ 
+    return message.channel.send(`${user.tag} has received ${pointsToAdd} points and now stands at ${userscore.points} points.`);
+  }
+ 
+  if(command === "leaderboard") {//Baron#1500
+    // Grab the//Baron#1500
+    const top10 = sql.prepare("SELECT * FROM scores WHERE guild = ? ORDER BY points DESC LIMIT 10;").all(message.guild.id);
+ 
+    // Now shake it and show it! (as a nice embed, too!)//Baron#1500
+    const embed = new Discord.RichEmbed()//Baron#1500
+      .setTitle("Leaderboard")//Baron#1500
+      .setAuthor(client.user.username, client.user.avatarURL)//Baron#1500
+      .setDescription("Our top 10 points leaders!")//Baron#1500
+ 
+    for(const data of top10) {//Baron#1500
+      embed.addField(client.users.get(data.user).tag, `${data.points} points (level ${data.level})`);  //Baron#1500
+    }
+    return message.channel.send({embed}); //Baron#1500
+  }//Baron#1500
+});//Baron#1500
 
 client.login(process.env.BOT_TOKEN);
